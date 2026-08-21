dc6be19ac82fa5872de50d95deb40282a7efc068
EricC-stackZ <ryuujitakasu1509@gmail.com>
2026-08-20 22:12:34 -0300

    Botão Livro de Receitas

--- Assets/AcromaCura 2026.2/Scripts/MenuInicial/UiScript.cs
+++ Assets/AcromaCura 2026.2/Scripts/MenuInicial/UiScript.cs
@@ -5,11 +5,18 @@ public class UiScript : MonoBehaviour
     [SerializeField] // Vetor de objetos vazios para manipulação da UI no canvas...
     GameObject[] menuInicial;
 
+    [SerializeField]
+    GameObject[] livroDeReceitas;
+
     public void IniciarJogo() // Método para desativar os botões do menu e a imagem de fundo...
     {
         menuInicial[0].SetActive(false);
 
         menuInicial[1].SetActive(false);
+
+        menuInicial[4].SetActive(true);
+
+        livroDeReceitas[1].SetActive(true);
     }
 
     public void MostrarCreditos() // Método para mostrar a tela de créditos e desativar os botões do menu...
@@ -38,6 +45,24 @@ public class UiScript : MonoBehaviour
         menuInicial[3].SetActive(false);
 
         menuInicial[0].SetActive(true);
+    }
+
+    public void MostrarLivro() // Método para mostrar a tela do livro de receitas...
+    {
+        livroDeReceitas[0].SetActive(true);
+
+        livroDeReceitas[1].SetActive(false);
+
+        livroDeReceitas[2].SetActive(true);
+    }
+
+    public void FecharLivro() // Método para fechar a tela do livro de receitas...
+    {
+        livroDeReceitas[0].SetActive(false);
+
+        livroDeReceitas[1].SetActive(true);
+
+        livroDeReceitas[2].SetActive(false);
     }
 
     public void FecharJogo() // Método para fechar/sair do jogo...
