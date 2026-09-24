f4638c304edfe95794ded6241bdcebfce4b172d5
EricC-stackZ <ryuujitakasu1509@gmail.com>
2026-09-24 13:18:44 -0300

    Módulo 2 (Completo)

--- Assets/Acrothecary 2026.2/Scripts/MenuInicial/UiScript.cs
+++ Assets/Acrothecary 2026.2/Scripts/MenuInicial/UiScript.cs
@@ -3,20 +3,16 @@ using UnityEngine;
 public class UiScript : MonoBehaviour
 {
     [SerializeField] // Vetor de objetos vazios para manipulação da UI no canvas...
-    GameObject[] menuInicial; 
+    GameObject[] menuInicial; // Índice 0 = Botões do Menu || Índice 1 = Canvas Principal do Menu || Índice 2 = Mostrar Créditos || Índice 3 = Canvas Secundário (Game)
 
     [SerializeField]
-    GameObject[] livroDeReceitas;
+    GameObject[] livroDeReceitas; // Índice 0 = Livro de Receitas Aberto || Índice 1 = Botão para abrir o Livro || Índice 2 = Botão Invisível para Fechar o Livro
 
     public void IniciarJogo() // Método para desativar os botões do menu e a imagem de fundo...
     {
-        menuInicial[0].SetActive(false);
-
         menuInicial[1].SetActive(false);
 
-        menuInicial[4].SetActive(true);
-
-        livroDeReceitas[1].SetActive(true);
+        menuInicial[3].SetActive(true);
     }
 
     public void MostrarCreditos() // Método para mostrar a tela de créditos e desativar os botões do menu...
@@ -29,20 +25,6 @@ public class UiScript : MonoBehaviour
     public void FecharCreditos() // Método para fechar a tela de créditos e reativar os botões do menu...
     {
         menuInicial[2].SetActive(false);
-
-        menuInicial[0].SetActive(true);
-    }
-
-    public void MostrarTutorial() // Método para mostrar a tela de tutorial e desativar os botões do menu...
-    {
-        menuInicial[3].SetActive(true);
-
-        menuInicial[0].SetActive(false);
-    }
-
-    public void FecharTutorial() // Método para fechar a tela de tutorial e reativar os botões do menu...
-    {
-        menuInicial[3].SetActive(false);
 
         menuInicial[0].SetActive(true);
     }
